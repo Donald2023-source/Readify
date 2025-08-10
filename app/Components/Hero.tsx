0c37aca73ba30ffaaf282e05eb573d27b99cd0ec
Donald-Source2023 <donalddyusuf@gmail.com>
2025-08-10 12:19:22 -0700

    hero

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -1,18 +1,23 @@
 import React from "react";
+import Button from "./Button";
 
 const Hero = () => {
   return (
-    <div className="h-full border">
+    <div className="h-full max-w-6xl flex items-center flex-col pt-10">
       <div className="flex items-center w-[55%] justify-center mx-auto flex-col gap-4 text-center">
-        <h2 className="text-4xl font-bold">
+        <h2 className="text-4xl font-bold leading-12">
           Transform Your Conversions With {" "}
           <span className="text-[#0157FF]">Readify AI</span>
-        </h2>
-        <p>
+        </h2> 
+      </div>
+
+      <div>
+        <p className="w-[70%] text-gray-400 text-center mx-auto py-5  flex leading-8">
           Discover the power of Readify - your ultimate personal AI document
           summarizer, designed to enhance communication and supercharge your
           productivity.
         </p>
+        <Button text="Try 7-day Free trial" />
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ const page = () => {
   return (
     <div>
       <Navbar />
-      <div className='max-w-6xl flex items-center mx-auto w-full  px-4'>
-        <div className='h-[80vh]'>
+      <div className=' flex items-center mx-auto justify-center w-full '>
+        <div className='h-[88vh] mx-auto flex items-center flex-col bg-gradient-to-b w-full from-white to to-[#0036a1] from-30%'>
           <Hero />
         </div>
       </div>
