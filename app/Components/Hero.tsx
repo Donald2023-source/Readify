8d747773c8e15787c86fe4d372832970b378540c
Donald-Source2023 <donalddyusuf@gmail.com>
2025-08-11 03:51:22 -0700

    hero

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Button from "./Button";
 import Image from "next/image";
@@ -5,17 +6,27 @@ import chrome from "@/public/Chrome.png";
 import product from "@/public/product.png";
 import { FaStar } from "react-icons/fa6";
 import heroImg from "@/public/Hero-image 1.png";
+import { motion } from "framer-motion";
 const Hero = () => {
   return (
     <div className="h-full max-w-6xl flex items-center flex-col pt-10">
-      <div className="flex items-center w-[80%] border md:w-[55%] justify-center mx-auto flex-col gap-4 text-center">
+      <motion.div
+        initial={{ x: -100, opacity: 0 }}
+        whileInView={{ x: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="flex items-center w-[95%]  md:w-[50%] justify-center mx-auto flex-col gap-4 text-center"
+      >
         <h2 className="md:text-4xl text-3xl font-bold leading-12">
-          Transform Your Conversions With{" "}
+          Transform Your Conversions{" "}
           <span className="text-[#0157FF]">Readify AI</span>
         </h2>
-      </div>
+      </motion.div>
 
-      <div>
+      <motion.div
+        initial={{ x: -100, opacity: 0.3 }}
+        whileInView={{ x: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
         <p className="md:w-[70%] w-[85%] text-gray-400 text-center mx-auto py-5  flex leading-8">
           Discover the power of Readify - your ultimate personal AI document
           summarizer, designed to enhance communication and supercharge your
@@ -25,7 +36,7 @@ const Hero = () => {
           className="block md:flex items-center justify-center mx-auto"
           text="Try 7-day free trial"
         />
-      </div>
+      </motion.div>
 
       <div className="flex items-center justify-center gap-20">
         <div className="flex items-center gap-4 mt-6 mx-auto">
