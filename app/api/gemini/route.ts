724a22e3eba71d2dfc77761df87ed68362f045a2
Donald-Source2023 <donalddyusuf@gmail.com>
2025-08-13 06:38:18 -0700

    chages

--- app/api/gemini/route.ts
+++ app/api/gemini/route.ts
@@ -1,14 +1,21 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { NextResponse } from "next/server";
+import pdfParse from "pdf-parse"
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 
 export async function POST(req: Request) {
   try {
-    const { prompt } = await req.json();
+    const formData = await req.formData();
+    const file = formData.get("file") as File;
+
+    if(!file) {
+      return NextResponse.json({ error: "No PDF uploaded"}, { status: 400})
+    }
+    const buffer = Buffer.from(await file.arrayBuffer());
+    const pdfData = await pdf
     const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
-    const result = await model.generateContent(prompt);
-    return NextResponse.json({ output: result.response });
+    
   } catch (err) {
     return NextResponse.json({ error: "Failed to generate Content" });
   }
